fb47860a06d6193b8339e45d20cfaafa9655bcb8
John <johnconley507@hotmail.com>
2016-08-25 23:18:05 -0400

    adding test cases.

--- port.js
+++ port.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-      // $('.parallax').parallax();
-      console.log("test'")
-    });
+// $(document).ready(function(){
+//       // $('.parallax').parallax();
+//       console.log("test'")
+//     });
         
-	
+// 	
+
+console.log("Test")
